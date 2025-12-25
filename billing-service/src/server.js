a348690e93f67787cd231c984e13525f6e9a3bde
Ajay Yadav <aj2872000@gmail.com>
2025-12-25 22:49:44 +0530

    added retry job for failed invoices

--- billing-service/src/server.js
+++ billing-service/src/server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 4004;
 
 const startServer = async () => {
   await initDB(); // initialize tables
+  
+  // 🔁 START RETRY JOB
+  require('./jobs/retry.job');
+  
   app.listen(PORT, () => {
     console.log(`Billing service running on port ${PORT}`);
   });
